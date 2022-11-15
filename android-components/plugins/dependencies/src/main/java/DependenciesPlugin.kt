b7a28ebedb87f6e31eb9c9974345b1348ab6d667
Christian Sadilek <christian.sadilek@gmail.com>
2022-11-15 15:10:59 -0500

    Make sure dependency plugin works for settings

--- android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
+++ android-components/plugins/dependencies/src/main/java/DependenciesPlugin.kt
@@ -2,14 +2,15 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import org.gradle.api.initialization.Settings
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 // If you ever need to force a toolchain rebuild (taskcluster) then edit the following comment.
 // FORCE REBUILD 2022-09-30
 
-class DependenciesPlugin : Plugin<Project> {
-    override fun apply(project: Project) = Unit
+class DependenciesPlugin : Plugin<Settings> {
+    override fun apply(project: Settings) = Unit
 }
 
 // Synchronized version numbers for dependencies used by (some) modules
